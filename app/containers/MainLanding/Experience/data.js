eee693b0ba6eac30875a319c3327997ee92fcf9b
PrudhviRaj5 <raj.prudhvi5@gmail.com>
2020-05-30 11:16:19 -0400

    modified experience section; seperated data in a json

---
+++ app/containers/MainLanding/Experience/data.js
@@ -0,0 +1,23 @@
+const data = {
+  careers: [
+    {
+      company: 'Zylotech',
+      fromDate: 'Mar 2017',
+      toDate: 'Present',
+      designation: 'Tech Lead',
+      content: 'Blah Blah Blah',
+      website: 'http://zylotech.com',
+    },
+    {
+      company: 'Quantum Graph',
+      fromDate: 'Mar 2017',
+      toDate: 'Present',
+      designation: 'Tech Lead',
+      content: 'Blah Blah Blah',
+      website: 'http://zylotech.com',
+    },
+  ],
+  education: [],
+};
+
+export default data;
--- app/containers/MainLanding/Experience/index.jsx
+++ app/containers/MainLanding/Experience/index.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import './experience.scss';
+
+
 const Experience = () => {
   return (
-    <div>Experince</div>
+    <div className="experience-container">
+      <div>Experience</div>
+
+      <div>Career</div>
+
+      <div className="main">
+        <div className="exp-left">
+          <span>Company 1</span>
+          <span>Date - Date</span>
+        </div>
+        <div className="exp-right">
+          <span>Fulltime - Role</span>
+          <span>Content from resume</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
