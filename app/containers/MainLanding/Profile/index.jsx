b305696b24dfd8a7b1ca1a6041053f949bcfbceb
Pompita Sarkar <pompita.sarkar@hp.com>
2020-06-02 01:18:19 +0530

    Minor Profile css changes

--- app/containers/MainLanding/Profile/index.jsx
+++ app/containers/MainLanding/Profile/index.jsx
@@ -8,18 +8,24 @@ const Profile = () => {
   return (
     <div className="content-center-page profile-page">
       <h1 className="profile-heading">Profile</h1>
-      <p className="profile-overview">
+      <p className="profile-overview secondary-color">
         I am a Senior Software Engineer
       </p>
       <div className="profile-description">
         <div className="profile-description-container">
           <div>
             <h2 className="profile-description-heading">About me</h2>
-            <p>
-              5+ years experienced Senior Software Engineer with a demonstrated history of working in AI-based Startups.
-              Passionate, value-driven engineer with experience leading cross-functional teams to plan, build, launch and manage tech innovations.
-              Blend technology skills with extensive Agile/Scrum experience, a marketing orientation and analytical abilities to evolve product strategy.
-              Prioritize and manage multiple projects with minor scope changes during the iterations.
+            <p className="secondary-color">
+              5+ years experienced Senior Software Engineer with a demonstrated history
+              of working in
+              AI-based Startups
+              Passionate, value-driven engineer with experience leading cross-functional
+              teams to plan, build, launch
+              and manage tech innovations.
+              Blend technology skills with extensive Agile/Scrum experience, a marketing orientation
+              and analytical abilities to evolve product strategy.
+              Prioritize and manage multiple projects with minor scope changes during the
+              iterations.
               Experienced in scaling applications from POC to production with minimal timelines.
             </p>
           </div>
@@ -37,21 +43,21 @@ const Profile = () => {
             <span>
               <strong>Name:</strong>
             </span>
-            <div>
+            <div className="secondary-color">
               Prudhvi Raj
             </div>
 
             <span>
               <strong>Age:</strong>
             </span>
-            <div>
+            <div className="secondary-color">
               28 years
             </div>
 
             <span>
               <strong>Location:</strong>
             </span>
-            <div>
+            <div className="secondary-color">
               Bengaluru, India
             </div>
           </div>
