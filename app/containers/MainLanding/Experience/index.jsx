af90be2f87a0aae64f21c2939ae9e478aa0db059
Pompita Sarkar <pompita.sarkar@hp.com>
2020-06-06 22:44:17 +0530

    Restructured Experience Section

--- app/containers/MainLanding/Experience/index.jsx
+++ app/containers/MainLanding/Experience/index.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 import React from 'react';
 import { Icon } from '@rmwc/icon';
 
 import data from './data.json';
 import './experience.scss';
 
+// const logo = require('assets/images/qgraph.png').default;
 
 const Experience = () => {
   console.log('data---', data);
@@ -15,19 +18,41 @@ const Experience = () => {
       </p>
       <div className="experience-description">
         {data.length > 0 && data.map((item) => {
+          const logo = require(`assets/images/${item.logo}`).default;
           return (
             <div className="experience-description-container">
               <div>
-                <h2 className="experience-description-heading skills-center">{item.company}</h2>
-                <span className="experience-description-timeline skills-center secondary-color">
-                  {`${item.fromDate} - ${item.toDate}`}
-                </span>
+                <h2 className="experience-description-heading">
+                  <img
+                    src={logo}
+                    alt={item.company}
+                    width="150"
+                    height="50"
+                  />
+                  <span className="experience-description-links experience-center">
+                    <span className="meta-info">
+                      <div>
+                        <Icon icon="location_on" className="location_icon" />
+                        {item.location}
+                      </div>
+                      <div className="experience-logo">
+                        <Icon icon="link" className="location_icon" />
+                        <a className="website-link" href={item.website} rel="noreferrer" target="_blank">{item.website}</a>
+                      </div>
+                    </span>
+                  </span>
+                </h2>
               </div>
               <div>
-                <h2 className="experience-description-heading skills-center">{item.designation}</h2>
+                <h2 className="experience-description-heading">
+                  {item.designation}
+                  <div className="experience-description-timeline secondary-color">
+                    {`${item.fromDate} - ${item.toDate}`}
+                  </div>
+                </h2>
                 {item.content.length > 0 && item.content.map((innerContent) => {
                   return (
-                    <div className="experience-description-content skills-center secondary-color">
+                    <div className="experience-description-content secondary-color">
                       <div>{innerContent.overview}</div>
                       <ul>
                         {innerContent.bullets.length > 0 && innerContent.bullets.map((bullet) => {
@@ -41,15 +66,6 @@ const Experience = () => {
                     </div>
                   );
                 })}
-                <span className="experience-description-links skills-center">
-                  <span className="meta-info">
-                    <Icon icon="location_on" className="location_icon" />
-                    {item.location}
-                    <span className="seperator">|</span>
-                    <Icon icon="link" className="location_icon" />
-                    <a className="website-link" href={item.website} rel="noreferrer" target="_blank">{item.website}</a>
-                  </span>
-                </span>
               </div>
             </div>
           );
